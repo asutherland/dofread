eca54dfda3b3d293080d18577c1be52c3e8187ad
Andrew Sutherland <asutherland@asutherland.org>
2008-05-18 01:38:37 -0400

    Rename things, give them a license, and refactor the mangling to be not quite so hardcoded, but still require someone to modify the code to mangle things.
    --HG--
    rename : doffer.kpf => dofread.kpf
    rename : doffer.py => dofread.py

--- dofread.py
+++ dofread.py
@@ -1,5 +1,42 @@
 #!/usr/bin/env python
 
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is the "dofread" D Object File reader.
+#
+# The Initial Developer of the Original Code is
+# Mozilla Messaging, Inc.
+# Portions created by the Initial Developer are Copyright (C) 2008
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Andrew Sutherland <asutherland@asutherland.org>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
 import macholib.MachO as Macho
 import macholib.mach_o as macho
 from macholib.ptypes import *
@@ -118,7 +155,11 @@ class dof_probe_t(Structure):
         ('pad2', p_ulong),
     )
 
-class Doffy(object):
+class DOFReader(object):
+    '''
+    A reasonably simple D Object File reader.
+    '''
+
     def __init__(self, filename):
         self.filename = filename
         self.mo = Macho.MachO(filename)
@@ -145,7 +186,7 @@ class Doffy(object):
                             dof_sections.append(section)
         return dof_sections
 
-    def load_dof(self, f, section):
+    def load_dof(self, f, section, EVIL_MANGLER=None):
         print 'Section:', section.sectname
         BASE_OFFSET = section.offset
         f.seek(section.offset)
@@ -181,9 +222,11 @@ class Doffy(object):
                     strtab_sec = dof_secs[provider.strtab]
                     f.seek(BASE_OFFSET + strtab_sec.offset)
                     strtab = f.read(strtab_sec.size)
-                    DO_EVIL = True
-                    if DO_EVIL:
-                        strtab_evil = strtab.replace('javascript', 'wavascript')
+                    if EVIL_MANGLER:
+                        strtab_evil = strtab
+                        for mangle_from, mangle_to in EVIL_MANGLER:
+                            strtab_evil = strtab_evil.replace(mangle_from,
+                                                              mangle_to)
                         f.seek(BASE_OFFSET + strtab_sec.offset)
                         f.write(strtab_evil)
                 
@@ -231,16 +274,20 @@ class Doffy(object):
                 print '(found %s)' % (SECTION_NAME_MAP[dof_sec.type],)
 
 
-    def get_dof_data(self):        
+    def get_dof_data(self, EVIL_MANGLER=None):        
         self.dofs = {}
-        f = open(self.filename, 'rb+')
+        f = open(self.filename, EVIL_MANGLER and 'rb+' or 'rb')
         for section in self.get_dof_sections():
-            dof = self.load_dof(f, section)
+            dof = self.load_dof(f, section, EVIL_MANGLER=EVIL_MANGLER)
         f.close()
 
 if __name__ == '__main__':
     import sys
-    patrick = Doffy(sys.argv[1])
-    patrick.sanity()
+    ## If you wanted to mangle things in the string table, you would do
+    ##  something like the following and pass it as EVIL_MANGLER to
+    ##  get_dof_data.
+    # EVIL_MANGLER = [('javascript', 'wavascript')]
+    dr = DOFReader(sys.argv[1])
+    dr.sanity()
     print '-' * 40
-    patrick.get_dof_data()
+    dr.get_dof_data()
